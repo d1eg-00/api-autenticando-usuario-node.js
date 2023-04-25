d23523ab398c6cfc72c8e867ea51c8fdb6c01bff
d1eg-00 <119086175+d1eg-00@users.noreply.github.com>
2023-04-25 01:26:28 +0000

    api funcionando

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ const path = require('path');
 const app = express();
 const porta = 443;
 
+app.use(session({secret:1234567890}))
 
+let login = 'admin';
+let senha = '1234';
+
+app.engine('html', require('ejs').renderFile)
+app.set('view wngine', 'html')
+app.set('views', patch.join(__dirname, './'))
+
+app.get('./', (req,res) => {
+  if(req.session.login){
+    res.render('logado')
+  }
+  else{
+    res.render('home')
+  }
+})
 
 app.listen(porta, () => {console.log('Servidor Rodando!')})
